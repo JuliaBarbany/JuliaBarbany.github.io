90f2a07bd08a34b0219b7ad2ef860e0282f47c37
Julipet Ba <julipetba@byod-111-176.pub.uniarts.fi>
2018-02-22 12:36:12 +0200

    added if statements

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -7,8 +7,23 @@ oriented = function(e) {
         }   else  { 
             compassHeading = e.alpha;
         }
+        if(compassHeading > 315 || compassHeading < 45) {
+                text_area.textContent = " a mug figure";
+        }
+
+        if(compassHeading >= 45 && compassHeading < 135) {
+                text_area.textContent = "lidl discount";
+        }
+
+        if(compassHeading >= 135 && compassHeading < 225) {
+                text_area.textContent = "shit made of plastic";
+        }
+
+        if(compassHeading >= 225 && compassHeading < 315) {
+                text_area.textContent = "false blood ";
+        }
         
-         text_area.textContent = compassHeading;
+         //text_area.textContent = compassHeading;
         //calls function to alter content based on heading
        // myOrientation();
     }
